3762644bd1f32883f011a70c9ff381a2989352f0
ChaseTuring <44014720+ChaseTuring@users.noreply.github.com>
2019-12-22 02:11:08 +0800

    #609(div2)

---
+++ codeforces/div2#609/A.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+const double eps = 1e-5;
+const int N = 1e5+10;
+
+void redirect() {
+    freopen("test.in","r",stdin);
+    freopen("test.out","w",stdout);
+}
+
+int main() {
+    redirect();
+    int n,a,b;
+    scanf("%d",&n);
+    printf("%d %d\n",9*n,8*n);
+}
